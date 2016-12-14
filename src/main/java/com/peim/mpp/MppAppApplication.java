89c42180b89cd4cf25e5069ab984b3017cb4914f
Ivan Petrov <ivanpetrov0005@gmail.com>
2016-12-14 20:26:15 +0300

    add project

---
+++ src/main/java/com/peim/mpp/MppAppApplication.java
@@ -0,0 +1,12 @@
+package com.peim.mpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MppAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MppAppApplication.class, args);
+	}
+}
